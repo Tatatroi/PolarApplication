272cbd629b8dca87963efedef5145bc2778bb1e8
Tatatroi <stefan.mitroi@stud.ubbcluj.ro>
2025-12-29 14:14:34 +0200

    Main Activity (at this point sensor should connect to the app)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("com.github.polarofficial:polar-ble-sdk:5.5.0")
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
     implementation(libs.androidx.ui)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/application/polarapplication/MainActivity.kt
+++ app/src/main/java/com/application/polarapplication/MainActivity.kt
@@ -11,12 +11,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.application.polarapplication.polar.PermissionHelper
+import com.application.polarapplication.polar.PolarManager
 import com.application.polarapplication.ui.theme.PolarApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var polarManager: PolarManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!PermissionHelper.hasAllPermissions(this)) {
+            PermissionHelper.requestAllPermissions(this)
+        }
+
+        // 2️⃣ Initializezi managerul Polar
+        polarManager = PolarManager(this)
+
+        // 3️⃣ Conectare la senzor (PUNE ID-UL TĂU AICI!)
+        polarManager.connectToDevice("A1234567")
+
+
         enableEdgeToEdge()
+
         setContent {
             PolarApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
