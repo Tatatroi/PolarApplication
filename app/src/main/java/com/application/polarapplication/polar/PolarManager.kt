e0bcbc99926a7078bad08ad8bc42541eee5cd7a4
Tatatroi <stefan.mitroi@stud.ubbcluj.ro>
2025-12-29 14:12:26 +0200

    Polar Manager implementation

--- app/src/main/java/com/application/polarapplication/polar/PolarManager.kt
+++ app/src/main/java/com/application/polarapplication/polar/PolarManager.kt
@@ -1,13 +1,18 @@
 package com.application.polarapplication.polar
 
-import com.polar.sdk.api.PolarBleApi
 import android.content.Context
-import com.polar.sdk.api.PolarBleApiDefaultImpl
-import com.polar.sdk.api.PolarBleApiCallback
 import android.util.Log
+import com.polar.sdk.api.PolarBleApi
+import com.polar.sdk.api.PolarBleApiCallback
+import com.polar.sdk.api.PolarBleApiDefaultImpl
 import com.polar.sdk.api.model.PolarDeviceInfo
+import com.polar.sdk.api.model.PolarHrData
+import io.reactivex.rxjava3.disposables.Disposable
 
 class PolarManager(context: Context) {
+
+    private var hrDisposable: Disposable? = null
+
     private val api: PolarBleApi by lazy {
         PolarBleApiDefaultImpl.defaultImplementation(
             context,
@@ -15,23 +20,36 @@ class PolarManager(context: Context) {
                 PolarBleApi.PolarBleSdkFeature.FEATURE_HR,
                 PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_SDK_MODE,
                 PolarBleApi.PolarBleSdkFeature.FEATURE_BATTERY_INFO,
-                PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_H10_EXERCISE_RECORDING,
-                PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_OFFLINE_RECORDING,
-                PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_ONLINE_STREAMING,
-                PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_DEVICE_TIME_SETUP,
-                PolarBleApi.PolarBleSdkFeature.FEATURE_DEVICE_INFO
+                PolarBleApi.PolarBleSdkFeature.FEATURE_POLAR_ONLINE_STREAMING
             )
         )
     }
 
     init {
         api.setApiCallback(object : PolarBleApiCallback() {
+
             override fun deviceConnected(polarDeviceInfo: PolarDeviceInfo) {
                 Log.d("POLAR", "Connected: ${polarDeviceInfo.deviceId}")
             }
 
             override fun deviceDisconnected(polarDeviceInfo: PolarDeviceInfo) {
                 Log.d("POLAR", "Disconnected: ${polarDeviceInfo.deviceId}")
+                hrDisposable?.dispose()
+            }
+
+            override fun hrFeatureReady(identifier: String) {
+                Log.d("POLAR", "HR feature ready for $identifier")
+
+                hrDisposable = api.startHrStreaming(identifier)
+                    .subscribe(
+                        { hrData: PolarHrData ->
+                            val hr = hrData.samples.last().hr
+                            Log.d("POLAR_HR", "Heart Rate: $hr bpm")
+                        },
+                        { error ->
+                            Log.e("POLAR_HR", "HR stream error: $error")
+                        }
+                    )
             }
         })
     }
@@ -43,5 +61,4 @@ class PolarManager(context: Context) {
     fun disconnectFromDevice(deviceId: String) {
         api.disconnectFromDevice(deviceId)
     }
-
 }
